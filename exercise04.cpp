b2eac661e62cd27ca4d7098f162496e9908c5279
Amandi Gunaratne <99731865+IT21326936@users.noreply.github.com>
2022-03-26 11:34:45 +0000

    Tutorial 3

--- exercise04.cpp
+++ exercise04.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int &length, int &width);
 
 // Do not change the main() function
 int main() {
    int length = 10, width = 5;
-   input(length, width);
-   print(length, width);
+   input(&length, &width);;
+   cout << print(length, width);
    return 0;
 }
 
@@ -19,3 +19,6 @@ void print(int len, int wth) {
 }
 
 // Implement the Input Function here
+void input(int &length, int &width){
+  return length, width;
+}
